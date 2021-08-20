c524d5b95d9ea9a37feb8bfc291c046adfe7a5fd
TexasVoloshin <karolina.woloszyn43@gmail.com>
2021-08-20 18:56:34 +0200

    POM elegant project exercise = locator setto namedtuble from collection

--- Python/Elegant_POM/pages/training_ground.py
+++ Python/Elegant_POM/pages/training_ground.py
@@ -1,16 +1,15 @@
 from selenium.webdriver.common.by import By
 from .base_element import BaseElement
 from .base_page import BasePage
-
+from .locator import Locator
 class TrainigGround(BasePage):
 
     url = 'https://techstepacademy.com/training-ground/'
 
     @property
     def button1(self):
-        locator = (By.ID, 'b1')
+        locator = Locator(by=By.ID, value='b1')
         return BaseElement(
             driver=self.driver,
-            by=locator[0],
-            value=locator[1]
+            locator=locator
         )
